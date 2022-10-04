2a9eff8f681e9949b9275a3d0f719aaa71dbe394
Ian Kelk <iankelk@gmail.com>
2022-10-03 21:54:17 -0400

    Change to animated graph

--- js/main.js
+++ js/main.js
@@ -1,131 +1,163 @@
-// Global constants
-const barWidth = 42;
-const barPadding = 6;
-const barRightOffset = 275;
-const textLeftOffset = 6;
-const textRightOffset = 10;
-const textTopOffset = 5;
-const units = ["height_m", "height_ft"];
+// SVG drawing area
+let margin = {top: 40, right: 10, bottom: 20, left: 80};
 
-// Global variables to allow unit switching
-let unit = "height_m";
-let current_index = 0;
+let width = 960 - margin.left - margin.right,
+	height = 500 - margin.top - margin.bottom;
 
-d3.csv("data/buildings.csv", (row) => {
-    // Convert numeric fields to numbers
-    row.height_ft = +row.height_ft;
-    row.height_m = +row.height_m;
-    row.height_px = +row.height_px;
-    row.floors = +row.floors;
-    return row
-}).then( (data) => {
-    // Sort the data
-    data.sort( (a,b) => b.height_m - a.height_m);
-    // Add an index to the data
-    for (let i = 0; i < data.length; i++) {
-        data[i].i = i;
-    }
-    // Log the current state of the data with the index
-    console.log(data)
+let svg = d3.select("#chart-area").append("svg")
+	.attr("width", width + margin.left + margin.right)
+	.attr("height", height + margin.top + margin.bottom)
+	.append("g")
+	.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    // Create the visualization barchart area
-    let svg = d3.select("#barchart")
-        .append("svg")
-        .attr('width', 550)
-        .attr('height', 500);
+// Scales
+let x = d3.scaleBand()
+	.rangeRound([0, width])
+	.paddingInner(0.1);
 
-    // Draw the bar chart and summary. Initialize it to current index which is initially 0
-    drawBarChart(svg, data);
-    showSummary(data[current_index]);
+let y = d3.scaleLinear()
+	.range([height, 0]);
 
-    // Get the toggle for the units (feet or meters)
-    let unit_toggle = document.querySelector('#units');
-    // Add a listener to the unit toggle to update the units and regenerate the summary
-    unit_toggle.addEventListener('change', (event) => {
-        unit = units[+unit_toggle.checked]
-        updateUnits();
-        showSummary(data[current_index])
-    });
+// Create the Axes
+let xAxis = d3.axisBottom()
+	.scale(x);
+let yAxis = d3.axisLeft()
+	.scale(y);
+
+// Draw the x axis
+let xGroup = svg.append("g")
+	.attr("class", "axis x-axis")
+	.attr("transform", `translate(0, ${height})`)
+
+// Draw the y axis
+let yGroup = svg.append("g")
+	.attr("class", "axis y-axis")
+
+// Create the Y axis label
+yGroup
+	.append("text")
+	.attr("transform", `rotate(-90)translate(${-height/2}, -300)`)
+	.attr("class", "axis-label y-label")
+	.attr("text-anchor", "middle")
+	.attr("x", 0)
+	.attr("y", 233)
+	.text("stores");
+
+// Initialize UI
+let initialRanking = d3.select("#ranking-type").property("value");
+let sortByCompanies = true;
+
+console.log(initialRanking);
+
+// Initialize data
+loadData();
+
+// Create a 'data' property under the window object
+// to store the coffee chain data
+Object.defineProperty(window, 'data', {
+	// data getter
+	get: function() { return _data; },
+	// data setter
+	set: function(value) {
+		_data = value;
+		// update the visualization each time the data property is set by using the equal sign (e.g. data = [])
+		updateVisualization()
+	}
 });
 
-function drawBarChart(svg, data) {
-    // Draw bars with click listener
-    let bars = svg.selectAll("rect")
-        .data(data)
-        .enter()
-        .append("rect")
-        .attr('x', barRightOffset)
-        .attr('y', (d, i) =>  (barWidth + barPadding) * i)
-        .attr('width', (d) => d.height_px )
-        .attr('height', barWidth)
-        .attr('class', 'building-bar')
-        .on('click', (event, d) => showSummary(d));
 
-    // Add building name labels with click listener
-    let buildingNames = bars.select("text.label-name")
-        .data(data)
-        .enter()
-        .append("text")
-        .attr('class', 'label-name')
-        .attr("x", () => barRightOffset - textRightOffset)
-        .attr("y", (d, i) => (barWidth + barPadding) * i + barWidth / 2 + textTopOffset)
-        .text((d) => d.building)
-        .on('click', (event, d) => showSummary(d));
+// Load CSV file
+function loadData() {
+	d3.csv("data/coffee-house-chains.csv").then(csv=> {
 
-    // Add height labels to the bars
-    let heightLabels = svg.selectAll("text.label-height")
-        .data(data)
-        .enter()
-        .append("text")
-        .attr('class', 'label-height')
-        .attr("x", (d) => barRightOffset + d.height_px - textLeftOffset)
-        .attr("y", (d, i) => (barWidth + barPadding) * i + barWidth / 2 + textTopOffset)
-        .text( (d) => d[unit].toLocaleString("en-US"));
+		csv.sort((a,b)=> b.stores - a.stores);
+
+		csv.forEach(function(d){
+			d.revenue = +d.revenue;
+			d.stores = +d.stores;
+		});
+
+		// Store csv data in global variable
+		data = csv;
+
+		// updateVisualization gets automatically called within the data = csv call;
+		// basically(whenever the data is set to a value using = operator);
+		// see the definition above: Object.defineProperty(window, 'data', { ...
+	});
 }
 
-// Update the building heights in the bar chart when units are changed
-function updateUnits() {
-    d3.selectAll("text.label-height").each(function(d, i) {
-        d3.select(this)
-            .text(d[unit].toLocaleString("en-US"));
-    });
+// option 1: D3
+d3.select("#ranking-type").on("change", updateVisualization);
+
+d3.select("#change-sorting").on("click", () => {
+	sortByCompanies = !sortByCompanies;
+	updateVisualization();
+});
+
+// Render visualization
+function updateVisualization() {
+
+	// Choose selection based on dropdown
+	let selection = d3.select("#ranking-type").property("value");
+	let filteredData = data.map(function(d){return {company: d.company, value: d[selection]}});
+
+	// Sort data based on sort button
+	let sortedData = sortData(filteredData,sortByCompanies);
+
+	// Update the scalers for the new data
+	x.domain(sortedData.map( (d) => d.company));
+	y.domain([0, d3.max(sortedData, (d) => d.value)]);
+
+	// Define the bar chart
+	let bar = svg.selectAll("rect")
+		// Custom key function
+		.data(sortedData, function(entry) { return entry.company; });
+		// Basic keys by index
+		//.data(sortedData);
+
+	// Update the axes
+	xAxis.scale(x)
+	yAxis.scale(y)
+
+	// Enter and update the bar chart
+	bar
+		.enter()
+		.append("rect")
+		.merge(bar)
+		.style("opacity", 0.5)
+		.transition()
+		.duration(1000)
+		.style("opacity", 1)
+		.attr("class", "bar")
+		.attr("x", (d) => x(d.company))
+		.attr("y", (d) => y(d.value))
+		.attr("width", x.bandwidth())
+		.attr("height", d=> height - y(d.value));
+
+	// Remove the old bar chart
+	bar.exit().remove();
+
+	// Update x axis
+	xGroup
+		.transition()
+		.duration(1000)
+		.call(xAxis)
+
+	// Update y axis
+	yGroup
+		.transition()
+		.duration(1000)
+		.call(yAxis)
+
+	// Append y axis label
+	svg.selectAll(".axis-label.y-label")
+		.text(selection);
 }
 
-// Display the summary on the side
-function showSummary(data) {
-    // Save the index of the current summary globally in case the units change
-    current_index = data.i;
-    // Get the height and height units dependong on the selection
-    let height = data[unit];
-    let height_unit = unit === "height_m" ? "m" : "ft";
-
-    // Create the HTML for the summary table with the various bootstrap classes for appearance
-    let summaryTable = `<h4>${data.building}</h4><br />`;
-    summaryTable += `<table class='table table-responsive table-striped table-hover table-borderless'>`;
-
-    summaryTable += `<tr><td class="header-cell">Height (${height_unit})</td>`;
-    summaryTable += `<td class="header-cell"><b>${height.toLocaleString("en-US")}</b></td></tr>`;
-
-    summaryTable += `<tr><td>City</td>`;
-    summaryTable += `<td><b>${data.city}</b></td></tr>`;
-
-    summaryTable += `<tr><td>Country</td>`;
-    summaryTable += `<td><b>${data.country}</b></td></tr>`;
-
-    summaryTable += `<tr><td>Floors</td>`;
-    summaryTable += `<td><b>${data.floors}</b></td></tr>`;
-
-    summaryTable += `<tr><td>Completed</td>`;
-    summaryTable += `<td><b>${data.completed}</b></td></tr>`;
-
-    summaryTable += `</tr></table></div>`;
-
-    // Add the summary table to the DOM under the element "building-summary" and "building-image"
-    document.getElementById("building-summary").innerHTML = summaryTable;
-    document.getElementById('building-image').setAttribute('src', `/img/${data.image}`);
-
-    // Add a wikipedia button with the link just the building name with spaces replaced with underscores
-    let wiki_button = document.getElementById('wikipedia');
-    wiki_button.setAttribute('href', `https://en.wikipedia.org/wiki/${data.building.replace(/ /g, '_')}`);
-    wiki_button.innerHTML = `Wikipedia article for ${data.building}`;
+function sortData(data, toggle){
+	if (toggle) {
+		return data.sort( (a,b) => b.value - a.value);
+	} else {
+		return data.sort( (a,b) => a.value - b.value);
+	}
 }
