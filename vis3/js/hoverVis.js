d6d7aa960101ac6d2d8904285422f08987ecb7df
Ian Kelk <iankelk@gmail.com>
2022-11-21 16:34:31 -0500

    Added hotspot locations

--- vis3/js/hoverVis.js
+++ vis3/js/hoverVis.js
@@ -10,13 +10,20 @@ class HoverVis {
     initVis() {
         let vis = this;
 
-        vis.width = 832;
-        vis.height = 900;
+        // vis.width = 832;
+        // vis.height = 900;
+
+        // define margins
+        vis.margin = {top: 20, right: 20, bottom: 20, left: 20};
+        vis.width = vis.parentElement.getBoundingClientRect().width - vis.margin.left - vis.margin.right;
+        vis.height = vis.parentElement.getBoundingClientRect().height - vis.margin.top - vis.margin.bottom;
 
         // init drawing area
         vis.svg = d3.select(vis.parentElement).append("svg")
-            .attr("width", vis.width)
-            .attr("height", vis.height);
+            .attr("width", vis.width + vis.margin.left + vis.margin.right)
+            .attr("height", vis.height + vis.margin.top + vis.margin.bottom)
+            .append('g')
+            .attr('transform', `translate (${vis.margin.left}, ${vis.margin.top})`);
 
         vis.image = vis.svg.append("svg:image")
             .attr("x", 0)
@@ -29,7 +36,7 @@ class HoverVis {
             .append("circle")
             .attr("cx", (d) => +d.x)
             .attr("cy", (d) => +d.y)
-            .attr("r", 40)
+            .attr("r", 12)
             .attr("fill", "green")
             .on("mouseover", function(event, d) {
                     d3.select(this)
