d80c69e0f0d8080b7ca8d6f79dee19d68b4cf864
RonanFonseca <54601025+RonanFonseca@users.noreply.github.com>
2022-11-30 16:15:36 -0300

    Updated globe visualization with sources
    Updated historyfake.tsv to include sources.
    Hovering on the images displays the sources for each.
    Fixed div centering for text displayed.
    Fixed resizing error for one of the images.

--- project/visuals/globe/js/textVis.js
+++ project/visuals/globe/js/textVis.js
@@ -3,13 +3,13 @@ function updateText(fakeNews){
 
     // Adds a description text based on the content of the fake news chosen
     myGlobeVis.description.html(`
-                         <div style="width:60vw; max-height:28vh; border-radius: 5px; padding: 5px">
+                         <div style="width:60vw; max-height:28vh; border-radius: 5px; padding: 5px; margin: auto;">
                              <h4>${fakeNews.event} <span style="font-size: 0.7em"> - ${formatYear(fakeNews.year)}, ${fakeNews.name}</span> <h3>   
                              <p class="news-description">${fakeNews.news}</p>   
                          </div>`);
 
     myGlobeVis.newsimage.html(`
-                    <img src="visuals/globe/img/${fakeNews.image_code}" alt="${fakeNews.alt}">
+                    <img src="visuals/globe/img/${fakeNews.image_code}" alt="${fakeNews.alt}" title="${fakeNews.img_credit}">
                     `
     );
 
