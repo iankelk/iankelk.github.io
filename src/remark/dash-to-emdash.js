de0c1a1e9774ea1b97f54d714ba22f524eae0431
Ian Kelk <iankelk@gmail.com>
2023-11-29 02:18:33 -0500

    Update dash-to-emdash.js

--- src/remark/dash-to-emdash.js
+++ src/remark/dash-to-emdash.js
@@ -3,8 +3,10 @@ import { visit } from 'unist-util-visit';
 const dashToEmdash = (options) => {
   const transformer = (ast) => {
     visit(ast, 'text', (node) => {
-      if (node.value) {
+      if (node.value && node.value.includes('--')) {
+        console.log('Before:', node.value);
         node.value = node.value.replace(/--/g, '—');
+        console.log('After:', node.value);
       }
     });
   };
