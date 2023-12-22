f674de8c81c74f9dd0bed226d264a7ac883d1e40
Ian Kelk <iankelk@gmail.com>
2023-12-22 09:42:56 -0500

    Fixed chat conversation dark mode

--- src/components/chat_conversation.jsx
+++ src/components/chat_conversation.jsx
@@ -1,19 +1,45 @@
 // src/components/chat_conversation.jsx
 
-import React from "react";
+import React from 'react';
+import { useColorMode } from '@docusaurus/theme-common';
 
 const ChatConversation = ({ conversation, userAvatar, chatbotAvatar }) => {
+  const { colorMode } = useColorMode();
+
+  const bubbleStyles = (speaker) => {
+    let backgroundColor;
+    let textColor;
+
+    if (colorMode === 'dark') {
+      backgroundColor = speaker === 'user' ? '#333' : '#1a4d57'; // Dark mode background colors
+      textColor = '#fff'; // Dark mode text color
+    } else {
+      backgroundColor = speaker === 'user' ? '#f0f0f0' : '#d1e7dd'; // Light mode background colors
+      textColor = '#000'; // Light mode text color
+    }
+
+    return {
+      backgroundColor,
+      color: textColor,
+      padding: '10px 15px',
+      borderRadius: '15px',
+      margin: '0 10px',
+      maxWidth: '70%',
+    };
+  };
+
   return (
     <div style={{
       fontFamily: '"Helvetica Neue", Helvetica, Arial, sans-serif',
       maxWidth: '90%',
       margin: 'auto',
+      color: colorMode === 'dark' ? 'var(--ifm-heading-color)' : 'inherit' // Use theme color for text
     }}>
       {conversation.map((entry, index) => (
         <div key={index} style={{
           display: 'flex',
-          alignItems: 'flex-end', // Align items to the bottom
-          flexDirection: entry.speaker === 'user' ? 'row' : 'row-reverse', // Reverse direction for the chatbot
+          alignItems: 'flex-end',
+          flexDirection: entry.speaker === 'user' ? 'row' : 'row-reverse',
           marginBottom: '10px',
         }}>
           <img
@@ -25,23 +51,17 @@ const ChatConversation = ({ conversation, userAvatar, chatbotAvatar }) => {
               borderRadius: '20px',
             }}
           />
-          <div style={{
-            backgroundColor: entry.speaker === 'user' ? '#f0f0f0' : '#d1e7dd',
-            padding: '10px 15px',
-            borderRadius: '15px',
-            margin: '0 10px',
-            maxWidth: '70%',
-          }}>
+          <div style={bubbleStyles(entry.speaker)}>
             <p style={{ margin: '0' }}>{entry.text}</p>
           </div>
           {entry.comment && (
             <div style={{
               flex: 1,
-              padding: '0 10px', // Give some spacing
+              padding: '0 10px',
               fontSize: '0.8em',
-              color: '#555',
+              color: colorMode === 'dark' ? '#ccc' : '#555', // Adjust comment color based on theme
               textAlign: entry.speaker === 'user' ? 'right' : 'left',
-              alignSelf: 'center', // Center align the comment
+              alignSelf: 'center',
             }}>
               {entry.comment}
             </div>
