2e027a8dbb7a003ee073c99092e39744d29efc68
Ian Kelk <iankelk@gmail.com>
2024-01-24 02:53:54 -0500

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -38,6 +38,12 @@ const config: Config = {
     [
       'classic',
       {
+        sitemap: {
+          changefreq: 'weekly',
+          priority: 0.5,
+          ignorePatterns: ['/tags/**'],
+          filename: 'sitemap.xml',
+        },
         docs: {
           sidebarPath: './sidebars.ts',
           remarkPlugins: [
